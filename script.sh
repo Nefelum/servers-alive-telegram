b2a63c998edae468af54ec6e7a983b6c20d5fea9
nefelum <adimas1672@gmail.com>
2023-01-29 14:35:20 +0400

    init

--- script.sh
+++ script.sh
@@ -1,7 +1,9 @@
 API_KEY=$1
 CHAT_ID=$2
+PATH_TO_IPS=$3
 
 function Ping {
+    sleep 5
     SERVERIP=$1
     ping -c 3 $SERVERIP > /dev/null 2>&1
     if [ $? -ne 0 ]
@@ -18,11 +20,8 @@ function InfiniteLoop {
     input_file=$1
     while IFS= read -r line
     do
-#        echo "$line"
-#        line=( $line )
-#        ip=${line[0]}
         Ping $line
     done < "$input_file"
 }
 
-InfiniteLoop "/home/.../ips"
+InfiniteLoop "$PATH_TO_IPS"
