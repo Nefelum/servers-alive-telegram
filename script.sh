d31bd644e3ef6558677f6890ba7df3418b83b56b
dmitriy <adimas1672@gmail.com>
2023-01-30 16:42:32 +0400

    Test new logic

--- script.sh
+++ script.sh
@@ -1,14 +1,15 @@
 API_KEY=$1
 CHAT_ID=$2
 PATH_TO_IPS=$3
+BAD_IPS_ARRAY=()
 
 function Ping {
     SERVERIP=$1
     sleep 10
-    ping -c 3 $SERVERIP > /dev/null 2>&1
+    ping -c 3 "$SERVERIP" > /dev/null 2>&1
     if [ $? -ne 0 ]
     then
-        text=$(echo "$output" | sed -r 's/[+]+/%2B/g')
+        BAD_IPS_ARRAY+=("$SERVERIP")
         text="Server $SERVERIP is down"
         curl -s -X POST https://api.telegram.org/bot$API_KEY/sendMessage -d "chat_id=$CHAT_ID" -d text="$text"
     else
@@ -22,9 +23,39 @@ function InfiniteLoop {
     do
         while IFS= read -r line
         do
-            Ping $line
+            SERVERIP=$line
+            IsDown=$(BadIpExists "$SERVERIP")
+            if [ "$IsDown" = "Server $SERVERIP was down" ]; then
+                sleep 5
+            else
+                Ping "$SERVERIP"
+            fi
         done < "$input_file"
     done
 }
+
+
+function BadIpExists {
+    check_ip=$1
+    for ip in "${BAD_IPS_ARRAY[@]}"
+    do
+        if [ "$check_ip" = "$ip" ]; then
+            echo "Server $check_ip was down"
+            return 1
+        fi
+    done
+    echo "Server $check_ip is up!"
+}
+
+
+#function CheckIp {
+#    check_ip=$1
+#    for ip in "${BAD_IPS_ARRAY[@]}"
+#    do
+#        if [ "$check_ip" = "$ip" ]; then
+#            echo "Server $check_ip was already down"
+#        fi
+#    done
+#}
 
 InfiniteLoop "$PATH_TO_IPS"
